694354ddbee15d857746286cc6f63fabe0072459
Leonardo Bloise <leonardocsbloise@gmail.com>
2023-12-16 17:11:55 -0300

    feat: Dynamic Array Implementation.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,14 @@
 fun main() {
     val arr = DynamicArray()
-    for (i in 0..5) {
+    for (i in 0..9) {
         arr.append(i)
     }
+    arr.delete(2)
+    arr.append(10)
+    arr.delete(0)
+    arr.append(11)
     println(arr)
     println(arr.size())
+    println(arr.get(2));
+    println(arr.get(3));
 }

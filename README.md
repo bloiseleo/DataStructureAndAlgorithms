64c7974c9491bb597410378df0c3c5dfcda1e144
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-01-04 23:37:07 -0300

    feat: Traversal Contains and Traversal print using no recursion.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ All the theory was learned from [this](https://www.youtube.com/watch?v=RBSGKlAvo
 - Dynamic Array: Structure in which can grow when trying to append more elements than the original can contain. Every time it happens, the array doubles in size.
 - Double Linked List: Structure in which does not depend on contiguous memory space and is entirely based on Pointers.
 - Queue: Structure in which can emulate a real-world queue.
-- Tree: Structure in which each node can hold N references to another nodes. One reference points to the parent and the others points to its children.
+- Tree: Structure in which each node can hold N references to another nodes. One reference points to the parent and the others points to its children. I'm implementing a Binary Search Tree.
 
 ## Exercises
 - Bracket exercise using Java Stack to learn more and play.
--- src/main/kotlin/tree/KTree.kt
+++ src/main/kotlin/tree/KTree.kt
@@ -1,8 +1,10 @@
 package tree
 
+import java.util.Stack
+
 class KTree {
-    private var root: KtreeNode? = null;
-    private var length = 0;
+    private var root: KtreeNode? = null
+    private var length = 0
     private fun hasRoot(): Boolean {
         return root != null
     }
@@ -16,20 +18,51 @@ class KTree {
         length++
         return
     }
-    fun printInorder(): String {
+    fun printInorderRecursive(): String {
         val sb = StringBuilder("")
         if (!hasRoot()) {
+            return sb.toString()
+        }
+        return root!!.printInOrder(sb).toString()
+    }
+    fun printInOrderWithouRecursion(): String {
+        val sb = StringBuilder("");
+        if(!hasRoot()) {
             return sb.toString();
         }
-        return root!!.printInOrder(sb).toString();
+        val s = Stack<KtreeNode>();
+        var curr = root;
+        while (curr != null || !s.isEmpty()) {
+            while (curr != null) {
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop()
+            sb.append(" $curr ");
+            curr = curr.right
+        }
+        return sb.toString();
     }
     fun contains(value: Int): Boolean {
         if (root == null)  {
             return false
         }
+        val s = Stack<KtreeNode>()
+        var curr = root
+        while (curr != null || !s.isEmpty()) {
+            while (curr != null) {
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop()
+            if (curr.getValue() == value) {
+                return true;
+            }
+            curr = curr.right
+        }
         return false;
     }
     override fun toString(): String {
-        return printInorder();
+        return printInOrderWithouRecursion();
     }
 }
